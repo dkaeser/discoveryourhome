23dacf2d0a73aa72d8b05591e45082a3f93f3c65
dkaeser <daniel.kaeser@ricardo.ch>
2018-12-14 14:22:15 +0100

    message

---
+++ precache-manifest.a78c67a497437719984ef8e392d8fed8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "fd600ae702ad8409fbe7",
+    "url": "/static/css/main.1ad845d4.chunk.css"
+  },
+  {
+    "revision": "5f86aedc643d95a9db9fe26785efe623",
+    "url": "/static/media/living-room-summary.5f86aedc.png"
+  },
+  {
+    "revision": "ba8537e38f4673abf29a",
+    "url": "/static/js/1.ba8537e3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "fd600ae702ad8409fbe7",
+    "url": "/static/js/main.fd600ae7.chunk.js"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "42a1fecf750f0d2be16c27572a2fc05b",
+    "url": "/static/media/intro-desktop.42a1fecf.png"
+  },
+  {
+    "revision": "fb8f003c827e19bc6580e19edd46fe55",
+    "url": "/static/media/living-room-small.fb8f003c.png"
+  },
+  {
+    "revision": "ada9cf8fc40daf307f5e99208209d995",
+    "url": "/static/media/ricardo-logo-small.ada9cf8f.png"
+  },
+  {
+    "revision": "2e77124a7a323f553ebef6c4fbfb9988",
+    "url": "/static/media/living-room.2e77124a.png"
+  },
+  {
+    "revision": "ba8537e38f4673abf29a",
+    "url": "/static/css/1.2f879e5c.chunk.css"
+  },
+  {
+    "revision": "9fa06dcee2cda5a85637c88b61e66a03",
+    "url": "/index.html"
+  }
+];
